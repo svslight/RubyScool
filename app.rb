6c06c9388afe55ea95d4c75ace10ff80cfd67a84
Svetlana Solodovnikova <svslight@yandex.ru>
2020-01-24 15:27:11 +0500

    Add name parameter validation

--- app.rb
+++ app.rb
@@ -3,15 +3,16 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  erb 'Hello all Ruby-programmers!'
+	erb 'Hello all Ruby-programmers!'
 end
 
 get '/about' do
-  erb :about
+	# @error = 'something wrong!!!'
+	erb :about
 end
 
 get '/visit' do
-   erb :visit
+	erb :visit
 end
 
 post '/visit' do
@@ -21,11 +22,16 @@ post '/visit' do
 	@barber = params[:barber]
 	@color = params[:color]
 
+	if @username == ''
+		@error = 'Ведите имя'
+		return erb :visit
+	end
+
 	erb "Ok! username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
 
 end
 
 get '/contacts' do
-   erb :contacts
+	erb :contacts
 end
 
